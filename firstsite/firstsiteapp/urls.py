ff5b6a88c745e59864be5aefff4faedb1dae1486
dprokofiev <dprokofiev@ya.ru>
2020-02-28 14:50:11 +0500

    init

--- firstsite/firstsiteapp/urls.py
+++ firstsite/firstsiteapp/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from firstsiteapp import views
+from .views import BookList
+from django.conf.urls import url
 
 
 app_name = 'firstsiteapp'
 
 urlpatterns = [
-    path('', views.main_view, name='index'),
+    path('', BookList.as_view(),  name='index'),
+   # path('', views.main_view, name='index'),
+    path('create/', views.create_book, name='create'),
+    path('book/<int:id>/', views.book, name='book')
     ]
