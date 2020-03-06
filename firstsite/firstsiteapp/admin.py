b05085565f372513c1b1e0904d7f5a4040719d0c
dprokofiev <dprokofiev@ya.ru>
2020-03-06 13:57:10 +0500

    init

--- firstsite/firstsiteapp/admin.py
+++ firstsite/firstsiteapp/admin.py
@@ -5,3 +5,13 @@ from .models import  Book, Year, Category
 admin.site.register(Book)
 admin.site.register(Year)
 admin.site.register(Category)
+
+"""
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('name', 'link', 'authors', 'year','category', 'language')
+    list_filter = ('name', 'authors', 'language', 'category', 'year')
+    search_fields = ('name', 'years', 'category')
+    prepopulated_fields = {'link': ('name',)}
+    raw_id_fields = ('authors',)
+"""
--- firstsite/firstsiteapp/urls.py
+++ firstsite/firstsiteapp/urls.py
@@ -9,6 +9,15 @@ app_name = 'firstsiteapp'
 urlpatterns = [
     path('', BookList.as_view(),  name='index'),
    # path('', views.main_view, name='index'),
-    path('create/', views.create_book, name='create'),
-    path('book/<int:id>/', views.book, name='book')
+    path('create/', views.create_book, name='create_link'),
+    path('db/', views.db, name='db_link'),
+    path('book/<int:id>/', views.book, name='book'),
+    path('cat-list', views.CatListView.as_view(), name='cat_list'),
+    path('cat-create/', views.CatCreateView.as_view(), name='cat_create'),
+    path('cat-edit/<int:pk>/', views.CatEditView.as_view(), name='cat_edit'),
+    path('cat-delete/<int:pk>/', views.CatDeleteView.as_view(), name='cat_delete'),
+    path('year-list', views.YearListView.as_view(), name='year_list'),
+    path('year-create/', views.YearCreateView.as_view(), name='year_create'),
+    path('year-edit/<int:pk>/', views.YearEditView.as_view(), name='year_edit'),
+    path('year-delete/<int:pk>/', views.YearDeleteView.as_view(), name='year_delete')
     ]
